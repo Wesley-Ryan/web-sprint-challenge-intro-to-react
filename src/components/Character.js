0447805853699440ffd4d5609b31be1053fd1bf7
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-09 08:41:50 -0700

    Passing each character to CharacterCard and rendering data

--- src/components/Character.js
+++ src/components/Character.js
@@ -10,9 +10,11 @@ const Character = (props) => {
     console.log("here are the results",results)
     return ( 
     <div className='character-container'>
-        {results.forEach((character) => { 
-            return <CharacterCard character={character} />
+        {results.map((character) => { 
+            console.log("myGuy", character)
+            return <CharacterCard character={character} key={character.id} />
         })}
+        
 
     </div> );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -6,10 +6,10 @@ const CharacterCard = (props) => {
     return (
         <div className="card">
             <h2 className="card-title">{character.name}</h2>
-            <img className='card-image'/>
-            <p className='gender'>Gender:</p>
-            <p className='location'>Location:</p>
-            <p className='status'>Status:</p>
+            <img src={character.image}className='card-image'/>
+    <p className='gender'>Gender: {character.gender}</p>
+            <p className='location'>Location: {character.location.name}</p>
+            <p className='status'>Status:{character.status}</p>
         </div>
       );
 }
