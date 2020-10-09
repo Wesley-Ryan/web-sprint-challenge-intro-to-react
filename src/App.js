d2c1597bbf36ff64a735dfccce8d05fdae885448
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-09 10:17:26 -0700

    Completed styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import Character from './components/Character'
 import './App.css';
+import RMBanner from './images/RMBanner.jpg'
+import styled from 'styled-components';
+
+const Banner = styled.div`
+  width:100%;
+  height:350px;
+  display:flex;
+  align-items: center;
+  border: 1px solid white;
+  background:no-repeat center center fixed; 
+  background-size: cover;
+  background-image: url(${RMBanner});
+  font-family: 'Montserrat', sans-serif;
+  font-size: 30px;
+  color: white;
+  text-shadow: 2px 2px 5px black;
+`
 
 const App = () => {
   const [characters, setCharacters] = useState([])
@@ -24,7 +41,9 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Rick and Morty Character List</h1>
+      <Banner>
+        <h1 className="Header">Rick and Morty Character List</h1>
+      </Banner>
       <Character results={characters} />
     </div>
   );
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,16 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 500px;
+  border-radius: 5px;
+  margin: 0 auto;
+  margin-bottom:20px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4);
+
+`
+
+const Content = styled.div`
+  align-self: center;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 14px;
+    `;
+
+const CardImage = styled.img`
+    width:30%;
+    padding:15px;
+    border-radius:15px;
+`
+
+const CardDetails = styled.div`
+    display: flex; 
+    flex-direction: column;
+
+`
+const CardText = styled.p` 
+    display:flex;
+    width:100%;
+    align-self: flex-start;
+`
 
 
 const CharacterCard = (props) => {
     const {character} = props
     return (
-        <div className="card">
+        <Container>
             <h2 className="card-title">{character.name}</h2>
-            <img src={character.image}className='card-image'/>
-    <p className='gender'>Gender: {character.gender}</p>
-            <p className='location'>Location: {character.location.name}</p>
-            <p className='status'>Status:{character.status}</p>
-        </div>
+            <Content>
+                <CardImage src={character.image}className='card-image'/>
+                <CardDetails>
+                <CardText>Gender: {character.gender}</CardText>
+                <CardText>Location: {character.location.name}</CardText>
+                <CardText>Status: {character.status}</CardText>
+                </CardDetails>
+            </Content>
+        </Container>
       );
 }
  
