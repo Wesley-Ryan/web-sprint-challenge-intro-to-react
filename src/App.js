6d07708d90b0b48abaea7f56788ae715c7ec6e64
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-09 08:13:48 -0700

    Fetching data from Rick and Morty API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [chracters, setCharacters] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() =>{ 
+    axios.get('https://rickandmortyapi.com/api/character')
+    .then((response) => {
+      console.log(response.data.results)
+    })
+    .catch((err) => { 
+      console.log("There was an error communicating with the server:", err)
+    })
+  }, [])
+
 
   return (
     <div className="App">
